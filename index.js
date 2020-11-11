ecf50b99c9f6bc3ed7dd63b44802809372768c82
Brendan Eggers <eggers.brendan@gmail.com>
2020-11-11 15:07:46 -0500

    Adding new

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const app = express();
 const { IncomingWebhook } = require("@slack/webhook");
 
 const url = process.env.SLACK_WEBHOOK_URL;
-const webhook = IncomingWebhook(url);
+const webhook = new IncomingWebhook(url);
 
 app.use(bodyParser.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
+
+app.get("/", () => {
+  console.log("here");
+});
 
 app.post("/slack-hooks", async (req) => {
   const { body } = req;
