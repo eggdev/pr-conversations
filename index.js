19cc543ecd314370faf8794ca890cc306e549cbd
Brendan Eggers <eggers.brendan@gmail.com>
2020-11-11 15:04:55 -0500

    logging the request again

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const { IncomingWebhook } = require("@slack/webhook");
+
+const url = process.env.SLACK_WEBHOOK_URL;
+const webhook = IncomingWebhook(url);
 
 app.use(bodyParser.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-app.post("/slack-hooks", (req, res) => {
-  console.log(req);
+app.post("/slack-hooks", async (req) => {
+  const { body } = req;
+  console.log(body);
+  //   await webhook.send({
+  //       text:
+  //   })
 });
 
 app.listen(port, () => console.log("were up"));
