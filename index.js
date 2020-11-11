904cfbefc51ea361ab3d2abe3309bd7608ca1d48
Brendan Eggers <eggers.brendan@gmail.com>
2020-11-11 14:32:41 -0500

    adding quick post method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("here");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.post("/slack-hooks", (req, res) => {
+  console.log(req);
+});
+
+app.listen(port, () => console.log("were up"));
